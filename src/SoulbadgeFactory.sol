7d1c84deae4d0676ffc4ca37fcaeb9fe7350e499
Yudhish Maran <66850474+0xYudhishthra@users.noreply.github.com>
2023-06-04 10:13:06 +0900

    fix: rename contract

--- src/SoulbadgeFactory.sol
+++ src/SoulbadgeFactory.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "./SoulBadge.sol";
+import "./Soulbadge.sol";
 
-contract CommitmentFactory {
+contract SoulbadgeFactory {
     Soulbadge[] public soulbadges;
     event SoulBadgeCreated(address soulbadge);
 
